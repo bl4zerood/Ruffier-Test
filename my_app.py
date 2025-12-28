07bdc8a8f574bb729582cc873516a5dfffaad47d
bl4zerood <bl4zerood@gmail.com>
2025-12-28 08:59:37 +0700

    import library

--- my_app.py
+++ my_app.py
@@ -1,1 +1,10 @@
-# write the code for main app and first screen
+from PyQt5.QtCore import Qt, QTimer, QTime, QLocale
+from PyQt5.QtGui import QDoubleValidator, QIntValidator, QFont
+from PyQt5.QtWidgets import (
+    QApplication, QWidget,
+    QHBoxLayout, QVBoxLayout, QGridLayout,
+    QGroupBox, QRadioButton,
+    QPushButton, QLabel, QListWidget, QLineEdit)
+
+from instr import *
+from second_win import *
